6aa8f14137207e71a0ba34595329de95241a6010
Jace Hull <jach1@umbc.edu>
2021-04-09 15:19:00 -0400

    Added unit tests

--- tests.py
+++ tests.py
@@ -1,7 +1,31 @@
+import unittest
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
-def test_get_observations():
-    pass
+class TestAPI(unittest.TestCase):
+    def test_get_observations(self):
+        q = {"lng": 0, "lat": 0}
+        response = client.get("/observations", params=q)
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_invalid_longitude(self):
+        q = {"lng": 182.0, "lat": 70.0}
+        response = client.get("/observations", params=q)
+        self.assertEqual(response.json(), { "err": "Invalid coordinates."})
+
+        q = {"lng": -190.0, "lat": 70.0}
+        response = client.get("/observations", params=q)
+        self.assertEqual(response.json(), { "err": "Invalid coordinates."})
+
+    def test_invalid_latitude(self):
+        q = {"lng": 70.0, "lat": 91.0}
+        response = client.get("/observations", params=q)
+        self.assertEqual(response.json(), { "err": "Invalid coordinates."})
+
+        q = {"lng": 70.0, "lat": -100.0}
+        response = client.get("/observations", params=q)
+        self.assertEqual(response.json(), { "err": "Invalid coordinates."})
