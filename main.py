46201ae8c45694b523f6fde044ff04ad68468b2d
Jace Hull <jach1@umbc.edu>
2021-04-09 15:18:49 -0400

    Updated observation query to have basic fields to query observations from inaturalist

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import requests
 import argparse
 
-from config import initialize_config
+import config_helper
 from fastapi import FastAPI
 from typing import Optional
 from dotenv import load_dotenv
@@ -12,19 +12,50 @@ load_dotenv()
 
 app = FastAPI()
 config = {}
-initialize_config(config)
+config_helper.initialize_config(config)
 
 
 @app.get("/observations")
-def get_observations(long: float, lat: float):
-    headers = {"Authentication": config['access_token']}
-    response = requests.get(f"{config['request_endpoint']}/users/your_user_here", headers=headers)
+def get_observations(lng: float, lat: float):
+    if not valid_lng_lat(lng, lat):
+        return { "err": "Invalid coordinates."}
+
+    headers = {
+        "Authentication": config['access_token'],
+        "Accept": "application/json"
+        }
+
+    q = {
+        "geo": "true",
+        "identified": "true",
+        "mappable": "true",
+        "verifiable": "true",
+        "identifications": "most_agree",
+        "photos": "true",
+        "native": "true",
+        "lat": lat,
+        "lng": lng,
+        "radius": 1,
+        "page": 1,
+        "per_page": 15,
+        "order": "desc",
+        "order_by": "created_at"
+    }
+    response = requests.get(f"{config['request_endpoint']}/observations", params=q, headers=headers)
+    
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Failed request: {response}")
+        print(response.text)
+        return { "err": f"Error retrieving request: {response.status_code}" }
 
 
+def valid_lng_lat(lng: float, lat: float):
+    if (abs(lng) > 180 or abs(lat) > 90): 
+        return False
+    return True
+
+@app.get("/refresh")
 def refresh_token():
     '''
     This function will request a new access_token, which is needed to make authenticated requests to iNaturalist.
