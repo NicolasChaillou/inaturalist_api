bda56a5c8f01ed00559bc3e6a136a02549b6da78
Jace Hull <jach1@umbc.edu>
2021-04-09 23:22:22 -0400

    Added threading so that access token is refreshed every 10 hours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import re
 import json
+import time
+import threading
 import requests
 import argparse
 
@@ -13,6 +15,26 @@ load_dotenv()
 app = FastAPI()
 config = {}
 config_helper.initialize_config(config)
+
+
+def refresh_token():
+    '''
+    This function will request a new access_token, which is needed to make authenticated requests to iNaturalist.
+    This access_token expires every 24 hours, so the function should be run at least once a day.
+    '''
+    # threading.Timer(60, refresh_token).start()
+    while True:
+        time.sleep(36000)
+        headers = { "Authorization": f"Bearer {config['access_token']}" }
+        response = requests.get(config["api_token_endpoint"], headers=headers)
+        if response.status_code == 200:
+            config["api_token"] = response.json()["api_token"]
+            print("Received new api_token")
+        else:
+            print("Failed to get new API token")
+
+refresh_thread = threading.Thread(target=refresh_token, daemon=True)
+refresh_thread.start()
 
 
 @app.get("/observations")
@@ -46,7 +68,6 @@ def get_observations(lng: float, lat: float):
     if response.status_code == 200:
         return response.json()
     else:
-        print(response.text)
         return { "err": f"Error retrieving request: {response.status_code}" }
 
 
@@ -54,16 +75,3 @@ def valid_lng_lat(lng: float, lat: float):
     if (abs(lng) > 180 or abs(lat) > 90): 
         return False
     return True
-
-@app.get("/refresh")
-def refresh_token():
-    '''
-    This function will request a new access_token, which is needed to make authenticated requests to iNaturalist.
-    This access_token expires every 24 hours, so the function should be run at least once a day.
-    '''
-    headers = { "Authorization": f"Bearer {config['access_token']}" }
-    response = requests.get(config["api_token_endpoint"], headers=headers)
-    if response.status_code == 200:
-        config["api_token"] = response.json()["api_token"]
-    else:
-        print(f"Failed to fetch new API token. Try refreshing your access token.\n{response}")
